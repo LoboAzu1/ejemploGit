d0dcbc109c9a78ac64043981b4105fae86b6c7fa
LoboAzu1 <57547367+LoboAzu1@users.noreply.github.com>
2022-12-16 20:58:53 -0600

    Se agregó archivo de Python.

---
+++ copyExamen.py
@@ -0,0 +1,19 @@
+
+print(" \n 1.-  Crea un arreglo de números aleatorios entre 3 y 5, cuya forma sea de 10x8x2 \n")
+
+
+print("2.- Utiliza booleanos como índices para imprimir los números del arreglo cuyo valor sea mayor que 4.7 \n")
+
+print("3.-  Encuentra el valor promedio del arreglo, calculado sobre la 3era dimensión (debes obtener 80 números) \n")
+
+print("\n")
+
+print("1.1.- Utiliza numpy para cargar el archivo de datos Top_secret.csv. Específicamente, del documento carga la columna con los valores Data_value y la columna con los valores para Period \n")
+
+print("2.1- A todos los elementos obtenidos de la columna Data_value, súmale un número aleatorio entre 0 y 100.\n")
+
+print("3.1 Aplica el método de mínimos cuadrados para ajustar una recta a dichos datos (x es Period e y es Data_value modificado, es decir, después del paso anterior).\n")
+
+print("4.1 Utiliza matplotlib para graficar los datos originales y la recta que encontraste para verificar que buscar una aproximación lineal es razonable en este caso. Tu gráfica debería tener etiquetas para las dos curvas, título en los ejes x e y, y un título general para la gráfica. ")
+
+print("")
